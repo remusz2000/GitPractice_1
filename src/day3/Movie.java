1429e17949c4e35dfdf7c8e5a09ccc30964991d6
remusz2000 <REMUSZ2000@GMAIL.COM>
2021-07-29 15:22:21 -0500

    added movie at line 10

--- src/day3/Movie.java
+++ src/day3/Movie.java
@@ -2,7 +2,13 @@ package day3;
 
 public class Movie {
 
-// after friendly discussion
+    // after friendly discussion
     //they have decided
     // to watch from 22 min
+
+    public static void main(String[] args) {
+        //Added some awesome things to movie class
+    }
+
+
 }
