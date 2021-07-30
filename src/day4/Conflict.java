d66294ede4b9523a7119449fc5262d9e2d642b39
remusz2000 <REMUSZ2000@GMAIL.COM>
2021-07-30 09:50:14 -0500

    master collection work is done

--- src/day4/Conflict.java
+++ src/day4/Conflict.java
@@ -7,5 +7,6 @@ public class Conflict {
 // This is the final version
     //Is the final merge result
  //This is collection branch.
+    // this is change collection2 branch does not know about
 
 }
